8728cf717a7490a1254ec6f478dd6b43cd5d7021
Wintang <wintang2003@icloud.com>
2024-01-09 14:53:29 +0700

    Add Entity and User

---
+++ AmazingTech.InternSystem/Data/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmazingTech.InternSystem.Data
+{
+    public abstract class Entity
+    {
+        [Key]
+        public string Id { get; set; }
+        public User CreatedBy { get; set; }
+        public User DeletedBy { get; set; }
+        public User LastUpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedTime { get; set; }
+        public DateTime DeletedTime { get; set; }
+    }
+}
---
+++ AmazingTech.InternSystem/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmazingTech.InternSystem.Data
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string Password { get; set; }
+        public string Sdt { get; set; }
+        public bool SdtXacNhan {  get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime DeletedTime { get; set; }
+        public DateTime LastUpdatedTime { get; set; }
+        public string ResetToken { get; set; }
+        public DateTime ResetTokenExpires { get; set; }
+        public string VerificationToken { get; set; }
+        public DateTime VerificationTokenExpires { get; set; }
+    }
+}
