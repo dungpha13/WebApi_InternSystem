942b73366d43d0320f6282aaa7639b032989d071
HSL-2003 <155443526+HSL-2003@users.noreply.github.com>
2024-01-13 03:33:40 +0700

    Add files via upload

---
+++ AmazingTech.InternSystem/Services/ViTriSerVice.cs
@@ -0,0 +1,27 @@
+﻿using AmazingTech.InternSystem.Data;
+using AmazingTech.InternSystem.Data.Entity;
+using AmazingTech.InternSystem.Repo;
+using AmazingTech.InternSystem.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazingTech.InternSystem.Services
+{
+    public class ViTriSerVice : IViTriService
+    {
+        private IViTriRepo _VitriRepo;
+        private readonly AppDbContext _context;
+        public ViTriSerVice(IServiceProvider serviceProvider, AppDbContext context) 
+        { 
+            _context = context;
+            _VitriRepo = serviceProvider.GetRequiredService<IViTriRepo>();
+        }
+
+        public List<ViTri> GetViTriList()
+        {
+            List<ViTri> viTris = _VitriRepo.GetViTriList();
+            return viTris;
+        }
+    }
+
+   
+}
