fdf634e4da6df76bd2d1707770a8b2a7f76f0b56
dungpha13 <dungphase@gmail.com>
2024-01-17 11:49:02 +0700

    Add validate data

--- AmazingTech.InternSystem/Models/Request/TruongHoc/AddTruongHocDTO.cs
+++ AmazingTech.InternSystem/Models/Request/TruongHoc/AddTruongHocDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AmazingTech.InternSystem.Models.Request.TruongHoc
 {
     public class AddTruongHocDTO
     {
+        [Required(ErrorMessage = "Name cannot be empty")]
         public string Ten { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "The number of internship weeks must be a positive integer")]
         public int SoTuanThucTap { get; set; }
     }
 }
--- AmazingTech.InternSystem/Models/Request/TruongHoc/UpdateTruongHocDTO.cs
+++ AmazingTech.InternSystem/Models/Request/TruongHoc/UpdateTruongHocDTO.cs
@@ -4,9 +4,13 @@ namespace AmazingTech.InternSystem.Models.Request.TruongHoc
 {
     public class UpdateTruongHocDTO
     {
-        [Required]
+
+        [Required(ErrorMessage = "Id is required")]
         public string Id { get; set; }
+
         public string? Ten { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "The number of internship weeks must be a positive integer")]
         public int? SoTuanThucTap { get; set; }
     }
 }
