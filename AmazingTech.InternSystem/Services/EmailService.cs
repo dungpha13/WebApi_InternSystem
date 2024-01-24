f77be903a8d38adc13ad8f11b5304829f019941b
Junnie2412 <junniehoang321@gmail.com>
2024-01-24 09:04:05 +0700

    Send Result Interview Email

--- AmazingTech.InternSystem/Services/EmailService.cs
+++ AmazingTech.InternSystem/Services/EmailService.cs
@@ -12,6 +12,7 @@ namespace AmazingTech.InternSystem.Services
     {
         public void SendMail(string Content, string ReceiveAddress, String Subject, string InternId);
         public void SendMail2(string Content, string ReceiveAddress, String Subject);
+        public void SendResultInterViewEmail(string Content, string ReceiveAddress, String Subject);
     }
 
     public class EmailService : IEmailService
@@ -71,6 +72,37 @@ namespace AmazingTech.InternSystem.Services
                 mailMessage.From = new MailAddress(EmailSettingModel.Instance.FromEmailAddress, EmailSettingModel.Instance.FromDisplayName);
                 mailMessage.To.Add(ReceiveAddress);
                 
+                var smtp = new SmtpClient()
+                {
+                    EnableSsl = EmailSettingModel.Instance.Smtp.EnableSsl,
+                    Host = EmailSettingModel.Instance.Smtp.Host,
+                    Port = EmailSettingModel.Instance.Smtp.Port,
+                };
+                var network = new NetworkCredential(EmailSettingModel.Instance.Smtp.EmailAddress, EmailSettingModel.Instance.Smtp.Password);
+                smtp.Credentials = network;
+
+                smtp.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void SendResultInterViewEmail(string Content, string ReceiveAddress, String Subject)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage()
+                {
+                    Subject = Subject,
+                    Body = Content,
+                    IsBodyHtml = true,
+                };
+                mailMessage.From = new MailAddress(EmailSettingModel.Instance.FromEmailAddress, EmailSettingModel.Instance.FromDisplayName);
+                mailMessage.To.Add(ReceiveAddress);
+
                 var smtp = new SmtpClient()
                 {
                     EnableSsl = EmailSettingModel.Instance.Smtp.EnableSsl,
