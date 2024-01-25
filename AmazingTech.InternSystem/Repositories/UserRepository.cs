df2550a3b47ca44602828ca7f049419d33568b35
Daiyun199 <yenphung3000@gmail.com>
2024-01-25 17:07:57 +0700

    fix validate create schedule

--- AmazingTech.InternSystem/Repositories/UserRepository.cs
+++ AmazingTech.InternSystem/Repositories/UserRepository.cs
@@ -11,7 +11,7 @@ namespace AmazingTech.InternSystem.Repositories
         public User GetUserByName(string name);
         public List<User> GetInternWithoutInterview(DateTime startTime, DateTime endTime);
         public List<User> GetHrOrMentorWithoutInterview(DateTime startTime, DateTime endTime);
-        public List<User> GetUsersWithoutInterview(DateTime startTime, DateTime endTime);
+        public List<User> GetInternsWithoutInterview(DateTime startTime, DateTime endTime);
     }
     public class UserRepository : IUserRepository
     {
@@ -93,7 +93,7 @@ namespace AmazingTech.InternSystem.Repositories
                 return result;
             }
         }
-        public List<User> GetUsersWithoutInterview(DateTime startTime, DateTime endTime)
+        public List<User> GetInternsWithoutInterview(DateTime startTime, DateTime endTime)
         {
             using (var context = new AppDbContext())
             {
@@ -106,17 +106,15 @@ namespace AmazingTech.InternSystem.Repositories
 
         public List<User> GetHrOrMentorWithoutInterview(DateTime startTime, DateTime endTime)
         {
-    
             using (var context = new AppDbContext())
             {
-                var result = context.Set<User>()
-              
-                      .Where(x => x.Roles.Any(r => r.Name.Equals(Roles.HR.ToUpper()) || r.Name.Equals(Roles.MENTOR)) && !context.Set<LichPhongVan>().AsNoTracking().Any(l => l.NguoiPhongVan.Id == x.Id && startTime <= l.ThoiGianPhongVan && l.ThoiGianPhongVan <= endTime))
-                      .Take(5)
-                      .ToList();
-                return result;
+                var usersWithoutInterview = context.Set<User>()
+                    .Where(user => !context.Set<LichPhongVan>().Any(interview => interview.NguoiPhongVan.Id == user.Id && startTime <= interview.ThoiGianPhongVan && interview.ThoiGianPhongVan <= endTime))
+                    .ToList();
+                return usersWithoutInterview;
             }
         }
+
     }
 
 }
