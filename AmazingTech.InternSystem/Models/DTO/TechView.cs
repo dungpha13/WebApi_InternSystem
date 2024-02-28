72b203a36986e11860390003796b7c176d777088
tân lê <tan81ht2016@gmail.com>
2024-02-28 09:47:52 +0700

    fix bug

--- AmazingTech.InternSystem/Models/DTO/TechView.cs
+++ AmazingTech.InternSystem/Models/DTO/TechView.cs
@@ -7,12 +7,6 @@ namespace AmazingTech.InternSystem.Models.DTO
     {
         public string Id { get; set; } 
         public string? Ten { get; set; }
-        public string? CreatedBy { get; set; }
-        public string? LastUpdatedBy { get; set; }
-        public string? DeletedBy { get; set; }
-        public DateTime? CreatedTime { get; set; }
-        public DateTime? LastUpdatedTime { get; set; }
-        public DateTime? DeletedTime { get; set; }
         public string IdViTri { get; set; }
         public string? ImgUrl { get; set; }
         public ViTri ViTri { get; set; }
