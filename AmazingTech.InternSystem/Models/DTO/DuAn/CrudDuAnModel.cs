caf5faaf58a25eb88ae8f714193e0273525d3864
Basil <144904034+DaoXuanQuy@users.noreply.github.com>
2024-02-28 11:05:55 +0700

    fix Validate DuAn

--- AmazingTech.InternSystem/Models/DTO/DuAn/CrudDuAnModel.cs
+++ AmazingTech.InternSystem/Models/DTO/DuAn/CrudDuAnModel.cs
@@ -1,10 +1,26 @@
-﻿namespace AmazingTech.InternSystem.Models.DTO.DuAn
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmazingTech.InternSystem.Models.DTO.DuAn
 {
-    public class CrudDuAnModel
+    public class CrudDuAnModel : IValidatableObject
     {
         public string? Ten { get; set; }
         public string LeaderId { get; set; }
+
+        [Required(ErrorMessage = "ThoiGianBatDau la bat buoc")]
+        [DataType(DataType.DateTime, ErrorMessage = "Sai kieu du lieu")]
         public DateTime? ThoiGianBatDau { get; set; }
+
+        [Required(ErrorMessage = "ThoiGianKetThuc la bat buoc")]
+        [DataType(DataType.DateTime, ErrorMessage = "Sai kieu du lieu")]
         public DateTime? ThoiGianKetThuc { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ThoiGianKetThuc < ThoiGianBatDau)
+            {
+                yield return new ValidationResult("ThoiGianKetThuc phai lon hon hoac bang ThoiGianBatDau", new[] { nameof(ThoiGianKetThuc) });
+            }
+        }
     }
 }
